3d207ff419faaf3b724c19b5e85da87809b84443
artgoblin <satabdadas3@gmail.com>
2026-05-01 03:22:59 +0530

    back end server up

--- dbsightFE/src/features/schema/baseQuery.js
+++ dbsightFE/src/features/schema/baseQuery.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { Navigate } from "react-router";
 
 export const authBaseQuery = fetchBaseQuery({
-  baseUrl: "/api",
+  baseUrl: "http://178.104.129.87:8080",
   prepareHeaders: (headers) => {
     const token = localStorage.getItem("authToken");
     if (token) {
